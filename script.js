728d14a1c1bd4f6906b713b6623cfc343c5a3cc6
hsu-shune-lai <hsushunelai.se@gmail.com>
2024-07-12 23:15:55 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let menu = document.querySelector("#menu-bars");
+  let navbar = document.querySelector(".navbar");
+  let searchIcon = document.querySelector("#search-icon");
+  let searchForm = document.querySelector(".search-form");
+
+  menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+    searchIcon.classList.remove("fa-times");
+    searchForm.classList.remove("active");
+  };
+
+  searchIcon.onclick = () => {
+    searchIcon.classList.toggle("fa-times");
+    searchForm.classList.toggle("active");
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+  };
+
+  window.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+    searchIcon.classList.remove("fa-times");
+    searchForm.classList.remove("active");
+  };
+});
